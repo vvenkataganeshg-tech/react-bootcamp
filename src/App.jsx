0d745f291a910c5546ca7e119a1073ca6edc5b38
vvenkataganeshg-tech <vvenkataganeshg@gmail.com>
2026-03-01 18:49:33 +0530

    version3

--- src/App.jsx
+++ src/App.jsx
@@ -34,13 +34,26 @@ function App(){
       {
         loading && <div className='alert alert-info'>Loading...</div>
       }
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
+      {
+        !loading && filteredPatients.length==0 &&(
+          <div className="col">
+            <div className='alert alert-warning text-center'>
+              No patient found
+            </div>
+          </div>
+        )
+      }
       {
         !loading && filteredPatients.map((i)=>(
-          <div className='col-md-4' key={i.id}>
+          <div className='col' key={i.id}>
             <PatientCard patient={i}/>
           </div>
         ))
       }
+
+      </div>
+      
     </div>
   )
 }
--- src/PatientCard.jsx
+++ src/PatientCard.jsx
@@ -1,7 +1,16 @@
-function PatientCard(patients){
+function PatientCard({ patient }) {
     return (
-        <div>
-            {patients.name}
+        <div class="card h-100 shadow-sm">
+            <div class="card-body">
+                <h5 class="card-title">Patient Name: {patient.name}</h5>
+                <p class="card-text"><strong>Weight:</strong>{patient.weight}</p>
+                <p class="card-text"><strong>Gender:</strong>{patient.gender}</p>
+                <p class="card-text"><strong>Age:</strong>{patient.age}</p>
+                <p class="card-text"><strong>Email:</strong>{patient.email}</p>
+                <p class="card-text"><strong>Disease:</strong>{patient.disease}</p>
+                <p class="card-text"><strong>Doctor Name:</strong>{patient.doctor.name}</p>
+                <p class="card-text"><strong>Doctor Specialization:</strong>{patient.doctor.specialization}</p>
+            </div>
         </div>
     )
 }
